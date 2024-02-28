6995801105143803bc3d37d591ce41d78ae1a5ae
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-02-28 09:35:41 -0500

    refactoring gameboard

--- src/components/game-board/GameBoard.jsx
+++ src/components/game-board/GameBoard.jsx
@@ -1,32 +1,37 @@
-// GameBoard.jsx
 import React from 'react';
-import GridContainer from '../grid/grid-container/GridContainer'; 
+import GridContainer from '../grid/grid-container/GridContainer';
 import GridRow from '../grid/grid-row/GridRow';
 import GridCell from '../grid/grid-cell/GridCell';
 
+// Function to determine pieceColor and hasBall
+const placePieces = (rowIndex, cellIndex) => {
+    let pieceColor = null;
+    let hasBall = false;
+
+    if (rowIndex === 0 && [2, 3, 4, 5].includes(cellIndex)) {
+        pieceColor = 'black';
+        hasBall = cellIndex === 4; // Black piece with the ball
+    } else if (rowIndex === 7 && [2, 3, 4, 5].includes(cellIndex)) {
+        pieceColor = 'white';
+        hasBall = cellIndex === 3; // White piece with the ball
+    }
+
+    return { pieceColor, hasBall };
+};
+
 const GameBoard = () => {
-  return (
-    <GridContainer>
-      {[...Array(8)].map((_, rowIndex) => (
-        <GridRow key={rowIndex}>
-          {[...Array(8)].map((_, cellIndex) => {
-            let pieceColor = null;
-            let hasBall = false;
-
-            if (rowIndex === 0 && [2, 3, 4, 5].includes(cellIndex)) {
-              pieceColor = 'black';
-              hasBall = cellIndex === 4;
-            } else if (rowIndex === 7 && [2, 3, 4, 5].includes(cellIndex)) { 
-              pieceColor = 'white';
-              hasBall = cellIndex === 3;
-            }
-
-            return <GridCell key={cellIndex} row={rowIndex} col={cellIndex} pieceColor={pieceColor} hasBall={hasBall} />;
-          })}
-        </GridRow>
-      ))}
-    </GridContainer>
-  );
+    return (
+        <GridContainer>
+            {[...Array(8)].map((_, rowIndex) => (
+                <GridRow key={rowIndex}>
+                    {[...Array(8)].map((_, cellIndex) => {
+                        const { pieceColor, hasBall } = placePieces(rowIndex, cellIndex);
+                        return <GridCell key={cellIndex} row={rowIndex} col={cellIndex} pieceColor={pieceColor} hasBall={hasBall} />;
+                    })}
+                </GridRow>
+            ))}
+        </GridContainer>
+    );
 };
 
 export default GameBoard;
