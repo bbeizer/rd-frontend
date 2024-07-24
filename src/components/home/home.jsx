ba27da5dd6b539771cad9a494320338c1b3e42e3
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-07-23 21:09:05 -0400

    added win img

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -66,32 +66,32 @@ function Home() {
       {waitingForPlayer && (
         <p className="waiting-text">Waiting for another player to join...</p>
       )}
-      <div class="rules">
+      <div className="rules">
         <h2>Game Rules</h2>
         <p><strong>Game Objective:</strong> The goal is to move one of your pieces holding the ball to the opponent's back rank.</p>
         <p><strong>Pieces and Movement:</strong></p>
         <ul>
           <li>Each player has 4 pieces.</li>
           <li>All pieces move like knights in chess (an L-shape).</li>
-            <div className="animation-container">
-              <img src="/images/attemptMove.png" alt="Attempt Move" className="move-animation" id="moveStep1"></img>
-              <img src="/images/makeMove.png" alt="Make Move" className="move-animation" id="moveStep2"></img>
-            </div>
+          <div className="animation-container">
+            <img src="/images/attemptMove.png" alt="Attempt Move" className="move-animation" id="moveStep1" />
+            <img src="/images/makeMove.png" alt="Make Move" className="move-animation" id="moveStep2" />
+          </div>
         </ul>
         <p><strong>The Ball:</strong></p>
         <ul>
           <li>One piece on each side starts with a metal ball.</li>
           <li>The piece holding the ball cannot move but can pass the ball to another piece.</li>
-          <img src ="/images/startPosition.png" id="startPos"></img>
+          <img src="/images/startPosition.png" alt="Start Position" className="static-image" id="startPos" />
         </ul>
         <p><strong>Passing the Ball:</strong></p>
         <ul>
           <li>The ball can be passed to any of the player's pieces that are laterally or diagonally adjacent.</li>
           <li>The path for passing the ball must be clear of opponent's pieces.</li>
           <div className="animation-container">
-              <img src="/images/start.png" alt="start" className="move-animation" id="pass1"></img>
-              <img src="/images/passAttempt1.png" alt="pass attempt 1" className="move-animation" id="pass2"></img>
-              <img src="/images/2nd.png" alt="pass1" className="move-animation" id="pass3"></img>
+            <img src="/images/start.png" alt="start" className="move-animation" id="pass1" />
+            <img src="/images/passAttempt1.png" alt="pass attempt 1" className="move-animation" id="pass2" />
+            <img src="/images/2nd.png" alt="pass1" className="move-animation" id="pass3" />
           </div>
         </ul>
         <p><strong>Gameplay:</strong></p>
@@ -99,11 +99,13 @@ function Home() {
           <li>On a turn, a player can either move one piece or pass the ball.</li>
           <li>The turn ends when the player presses the "Pass Turn" button.</li>
           <li>Turns alternate between players.</li>
-          <img src ="/images/passTurn.png" id="passTurn"></img>
+          <img src="/images/passTurn.png" alt="Pass Turn" className="static-image" id="passTurn" />
         </ul>
         <p><strong>Winning the Game:</strong> The first player to move a piece holding the ball to the opponent's back rank wins the game.</p>
+        <ul>
+          <img src="/images/win.png" alt="win" className="static-image" id="win" />
+        </ul>
       </div>
-
     </div>
   );
 }
