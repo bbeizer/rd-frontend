b42d7543162376a1e402a20fbb9ee63c19b71f90
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-10-02 22:10:14 -0400

    fixed perspective for single player mode as person is always white

--- src/components/game-board/helpers/fetchGame.js
+++ src/components/game-board/helpers/fetchGame.js
@@ -3,11 +3,14 @@ import { getGameById } from '../../../services/gameService';
 export async function fetchGame(gameId, updateState, currentPlayerColor) {
   try {
     const fetchedGame = await getGameById(gameId);
+    const isSinglePlayer = fetchedGame.gameType === 'single';
+    const playerColor = isSinglePlayer ? 'white' : currentPlayerColor;
     updateState(prevState => ({
       ...prevState,
       gameData: fetchedGame,
       gameType: fetchedGame.gameType,
       winner: fetchedGame.winner,
+      playerColor: playerColor,
       isUserTurn: fetchedGame.currentPlayerTurn === currentPlayerColor,
       currentBoardStatus: fetchedGame.currentBoardStatus
     }));
