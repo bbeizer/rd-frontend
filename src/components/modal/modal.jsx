74bb39e174041e478babf3836fed9ff9fe08894f
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2025-02-21 23:55:13 -0500

    added the css to the modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import './modal.css';
-const Modal = ({ children }) => {
-=======
 // components/Modal.js
+import './modal.css';
 const Modal = ({children}) => {
->>>>>>> 6413ac6 (fixed the refreshed issue with isUserTurn in singlePlayer)
     return (
         <div className="modal-background">
             <div className="modal-content">
